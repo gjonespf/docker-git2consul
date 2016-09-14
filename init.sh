583117c7da8c3c42ed853c907ac3964986d9a45c
gjones.pf@gmail.com <gjones.pf@gmail.com>
2016-09-14 16:46:49 +1200

    Add SOURCEROOT, ID encoding

--- init.sh
+++ init.sh
@@ -11,14 +11,24 @@ sed -i -e "s|GIT_REPO|$GIT_REPO|" /etc/git2consul.d/config.json
 #cat /etc/git2consul.d/config.json
 sed -i -e "s|NAMESPACE|$NAMESPACE|" /etc/git2consul.d/config.json
 #cat /etc/git2consul.d/config.json
+sed -i -e "s|GIT_SOURCEROOT|$GIT_SOURCEROOT|" /etc/git2consul.d/config.json
+
+if [ -n "$IDENC" ]
+then
+  ID=$(echo $IDENC |base64 -d)
+  IDPUB=$(echo $IDPUBENC |base64 -d)
+fi
 
 if [ -n "$ID" ]
 then
   mkdir ~/.ssh
-  echo $ID   |base64 -d > ~/.ssh/id_rsa
-  echo $IDPUB|base64 -d > ~/.ssh/id_rsa.pub
+#  echo $ID   |base64 -d > ~/.ssh/id_rsa
+#  echo $IDPUB|base64 -d > ~/.ssh/id_rsa.pub
+  echo $ID   > ~/.ssh/id_rsa
+  echo $IDPUB > ~/.ssh/id_rsa.pub
   echo -e "StrictHostKeyChecking no\nUserKnownHostsFile=/dev/null" > ~/.ssh/config
   chmod 700 -R ~/.ssh
+  ssh-add -l
 fi
 echo -e "$(date) starting git2consul. found these env vars: \nCFG:$CFG \nGIT_REPO:$GIT_REPO \nIDPUB:$IDPUB \nCONSUL_ENDPOINT:$CONSUL_ENDPOINT \nCONSUL_PORT:$CONSUL_PORT"
 #cat /etc/git2consul.d/config.json
